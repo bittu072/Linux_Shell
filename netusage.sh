c1191c8dc6cd0f8aaa6b32e66e3e19d2ec08e72c
bittu072 <bittu.patel072@gmail.com>
2017-04-28 20:51:32 -0700

    updated script to store infromation separately

--- netusage.sh
+++ netusage.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # path to save info of used data
-path1=/home/computer/logs/infolog
+path1=/home/bittu/computer/logs/infolog
+path2=/home/bittu/computer/logs/RxTxlog
 
 a=10
 
@@ -11,7 +12,14 @@ do
         now=$(date +"%T")
         var1="$(ifconfig wlp4s0)"
 	# replace "wlp4s0" with your network interface
+	# saves all the info of that interface
         echo "$var1" >> "$path1"
 
-        sleep 600
+	#save just RX and TX of the interface
+	info="$(grep "RX bytes" $path1)"
+	echo "time : $now" >> "$path2"
+	echo "$info" >> "$path2"
+	echo " " >> "$path2"
+
+	sleep 600
 done
