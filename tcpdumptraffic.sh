5b168f83c3851eaf2ec6962730f256c6f335616c
bittu072 <bittu.patel072@gmail.com>
2017-05-02 22:45:10 -0700

    added script to log the traffice using tcpdump

--- tcpdumptraffic.sh
+++ tcpdumptraffic.sh
@@ -2,3 +2,15 @@
 
 ## to install tcpdump
 # apt-get install tcpdump
+
+
+a=10
+
+until [ $a -lt 10 ]
+do
+        now=$(date +"%T")
+        echo "time : $now" >> "/home/bittu/computer/logs/trafficlog"
+	# -c flag will check traffic for 30 seconds
+        tcpdump -i wlp4s0 -c 30 >> /home/bittu/computer/logs/trafficlog
+        sleep 3600
+done
