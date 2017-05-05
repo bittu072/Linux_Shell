64fe12b660aae0f2c8fb6ca3eced010764decf7e
bittu072 <bittu.patel072@gmail.com>
2017-05-04 21:44:03 -0700

    added comments and reference command

--- tcpdumptraffic.sh
+++ tcpdumptraffic.sh
@@ -12,6 +12,8 @@ do
         echo "time : $now" >> "/home/bittu/computer/logs/trafficlog"
 	# -c flag will check traffic for 30 seconds
 	# added filter for the traffic
+	# also can be done
+	# tcpdump -i wlp4s0 src 192.168.0.1 or dst 192.168.0.1 -c 30 >> /home/bittu/computer/logs/trafficlog
         tcpdump -i wlp4s0 src 192.168.0.1 and not dst 192.168.0.1 -c 30 >> /home/bittu/computer/logs/trafficlog
         sleep 3600
 done
