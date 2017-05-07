b4a87091a730faccfb3d7d7ead9904e18f8b8795
bittu072 <bittu.patel072@gmail.com>
2017-05-07 16:20:38 -0700

    updated netusage script

--- netusage.sh
+++ netusage.sh
@@ -10,15 +10,15 @@ a=10
 until [ $a -lt 10 ]
 do
         now=$(date +"%T")
-        var1="$(ifconfig wlp4s0)"
+        tempvar="$(ifconfig wlp4s0)"
 	# replace "wlp4s0" with your network interface
 	# saves all the info of that interface
-        echo "$var1" >> "$path1"
+        echo "$tempvar" >> "$path1"
 
 	#save just RX and TX of the interface
-	info="$(grep "RX bytes" $path1)"
+	inf="$(grep "RX bytes" $path1)"
 	echo "time : $now" >> "$path2"
-	echo "$info" >> "$path2"
+	echo "$inf" >> "$path2"
 	echo " " >> "$path2"
 
 	sleep 600
