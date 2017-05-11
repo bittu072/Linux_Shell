e1de6af522d3dd9f4cf53e989717d3b40862eee5
bittu072 <bittu.patel072@gmail.com>
2017-05-10 20:52:53 -0700

    added comments

--- knowmac.sh
+++ knowmac.sh
@@ -2,5 +2,6 @@
 
 
 mac1="$(ifconfig wlp4s0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')"
+# replace wlp4s0 with other interface.. wlp4s0 usually shows wireless mac address
 
 echo "$mac1"
